9da90f7f2eb0ac690e7d4ce311552fa1b8593a93
Elaina <elainakat@gmail.com>
2020-09-25 15:25:41 -0600

    Create With Code Unit 1 Lessons

---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        // Player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        
+        //Turn
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
