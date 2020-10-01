fa34a6dd8e13d3241ee4dd7575f2ceecc434c079
Elaina <elainakat@gmail.com>
2020-10-01 15:46:19 -0600

    Create With Code 10:00am - 3:45pm

--- Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -19,7 +19,8 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
-            Destroy(gameObject);
+            Debug.Log("Game Over!");
+            //Destroy(gameObject);
         }
     }
 }
--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -7,8 +7,8 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20.0f;
     private float spawnPosZ = 20.0f;
-    private float startDelay = 2.0f;
-    private float spawnInterval = 1.5f;
+    private float startDelay = 0.5f;
+    private float spawnInterval = 0.5f;
     void Start()
     {
         InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
@@ -22,6 +22,7 @@ public class SpawnManager : MonoBehaviour
     void SpawnRandomAnimal()
     {
         Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        
         int animalIndex = Random.Range(0, animalPrefabs.Length);
         Instantiate(animalPrefabs[animalIndex], spawnPos,
             animalPrefabs[animalIndex].transform.rotation);
