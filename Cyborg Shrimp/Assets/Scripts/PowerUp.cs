23da90fb1e5763d2d47793171bf952f4610909ef
Elaina <elainakat@gmail.com>
2020-11-03 15:27:42 -0700

    Cyborn Shirp 2:30pm - 3:30pm

--- Cyborg Shrimp/Assets/Scripts/PowerUp.cs
+++ Cyborg Shrimp/Assets/Scripts/PowerUp.cs
@@ -1,11 +1,23 @@
 ﻿
 
+using System;
 using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
     public int score;
     public UnityEvent powerUpEvent;
+    public float gravity = -9.81f;
+    private Vector3 moveDirection;
+    private float yDirection;
+
+
+    private void Update()
+    {
+        //Gravity
+        yDirection += gravity * Time.deltaTime;
+
+    }
 
     private void OnTriggerEnter(Collider other)
     {
--- Cyborg Shrimp/Assets/Scripts/SpawnManager.cs
+++ Cyborg Shrimp/Assets/Scripts/SpawnManager.cs
@@ -6,10 +6,11 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject baddiePrefab;
     private Vector3 spawnPos = new Vector3(0,0,0);
+    private float repeatRate = 1;
     
     void Start()
     {
-        InvokeRepeating("SpawnBaddie", 0, 0);
+        InvokeRepeating("SpawnBaddie", 0, repeatRate);
     }
 
     void Update()
@@ -19,6 +20,7 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnBaddie()
     {
+        spawnPos = new Vector3(750, 30, 250);
         Instantiate(baddiePrefab, spawnPos, baddiePrefab.transform.rotation);
     }
 }
